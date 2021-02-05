27710995fcdc760ac0e48fef11b3e63c1380f4f4
vistritpandey <vistritpandey@gmail.com>
2021-02-05 16:39:52 +0530

    firebase and material ui added

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA05oLi89jPu2JrGf60omQbnVwlmNhGHcA",
+  authDomain: "vistrit-drive.firebaseapp.com",
+  projectId: "vistrit-drive",
+  storageBucket: "vistrit-drive.appspot.com",
+  messagingSenderId: "1019721713372",
+  appId: "1:1019721713372:web:5be1934ac5668733bd1619",
+  measurementId: "G-FY3FPSQ308",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
