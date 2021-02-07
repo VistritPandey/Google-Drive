bbd948f9e130a3ab671e3ad4490b1772951bf372
vistritpandey <vistritpandey@gmail.com>
2021-02-07 06:22:56 +0530

    k

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,11 @@
 import React from "react";
+import "../../styles/Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import SettingsIcon from "@material-ui/icons/Settings";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
 
 function index() {
   return (
@@ -11,7 +18,17 @@ function index() {
         <span>Drive</span>
       </div>
       <div className="header__searchContainer"></div>
-      <div className="header__icons"></div>
+      <SearchIcon />
+      <input type="text" placeholder="Search in Drive" />
+      <ExpandMoreIcon />
+      <div className="header__icons">
+        <span>
+          <HelpOutlineIcon />
+          <SettingsIcon />
+        </span>
+        <AppsIcon />
+        <Avatar />
+      </div>
     </div>
   );
 }
