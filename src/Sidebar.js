9b063937bbf8c4cfb5625a47a7211d6bc947cae5
vistritpandey <vistritpandey@gmail.com>
2021-02-09 15:28:59 +0530

    icons added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,28 @@
 import React from "react";
 import NewFile from "./NewFile";
 import "./Sidebar.css";
+import SidebarItem from "./SidebarItem";
+import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
+import ImportantDevicesIcon from "@material-ui/icons/ImportantDevices";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import QueryBuilderIcon from "@material-ui/icons/QueryBuilder";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import StorageIcon from "@material-ui/icons/Storage";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <NewFile />
       <div className="sidebar__itemsContainer">
+        <SidebarItem arrow icon={<InsertDriveFileIcon />} label={"My Drive"} />
+        <SidebarItem arrow icon={<ImportantDevicesIcon />} label={"Computer"} />
+        <SidebarItem icon={<PeopleAltIcon />} label={"Shared with me"} />
+        <SidebarItem icon={<QueryBuilderIcon />} label={"Recent"} />
+        <SidebarItem icon={<StarBorderIcon />} label={"Starred"} />
+        <SidebarItem icon={<DeleteOutlineIcon />} label={"Bin"} />
         <hr />
+        <SidebarItem icon={<StorageIcon />} label={"Storage"} />
       </div>
     </div>
   );
--- src/SidebarItem.js
+++ src/SidebarItem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 
-function SidebarItem() {
-  return <div className="Sidebar__item"></div>;
+function SidebarItem({ arrow, icon, label }) {
+  return (
+    <div className="sidebarItem">
+      <div className="sidebarItem__arrow">{arrow && <ArrowRightIcon />}</div>
+      <div className="sidebarItem__main">
+        {icon}
+        <p>{label}</p>
+      </div>
+    </div>
+  );
 }
 
 export default SidebarItem;
