ec589a07777899aff9c1fa89cf11718a1629d09b
vistritpandey <vistritpandey@gmail.com>
2021-02-10 15:31:56 +0530

    File View

---
+++ src/FilesView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FilesView.css";
+
+function FilesView() {
+  return (
+    <div className="fileView">
+      <p>Files</p>
+    </div>
+  );
+}
+
+export default FilesView;
