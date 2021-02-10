c6079aef62cf29fd865481d0b233758bbb838347
vistritpandey <vistritpandey@gmail.com>
2021-02-10 15:40:36 +0530

    Firebase added

--- src/FilesView.js
+++ src/FilesView.js
@@ -3,8 +3,17 @@ import "./FilesView.css";
 
 function FilesView() {
   return (
-    <div className="fileView">
-      <p>Files</p>
+    <div className="filesView">
+      <div className="filesView__row"></div>
+      <div className="filesView__titles">
+        <div className="filesView__titles--left">
+          <p>Name</p>
+        </div>
+        <div className="filesView__titles--right">
+          <p>Last Modified</p>
+          <p>File Size</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -14,5 +14,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebaseApp.storage();
 
-export { db, auth, provider };
+export { db, auth, provider, storage };
