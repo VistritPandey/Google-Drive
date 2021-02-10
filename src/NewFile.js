db708c8cfb037f0fe38eadaf615e2bd8ea24dcc9
vistritpandey <vistritpandey@gmail.com>
2021-02-10 19:35:15 +0530

    k

--- src/NewFile.js
+++ src/NewFile.js
@@ -1,14 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import "./NewFile.css";
+import firebase from "firebase";
+import { storage, db } from "./firebase.js";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  return {
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function NewFile() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+  const [file, setFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    setUploading(true);
+    storage
+      .ref(`files/${files.name}`)
+      .put(files)
+      .then((snapshot) => {
+        console.log(snapshot);
+        storage
+          .ref("files")
+          .child(file.name)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("myFiles").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: file.name,
+              fileUrl: url,
+              size: snapshot._delegate.bytesTransferred,
+            });
+          });
+      });
+  };
+
   return (
     <div className="newFile">
-      <div className="newFile__container">
+      <div className="newFile__container" onClick={handleOpen}>
         <AddIcon />
         <p>New</p>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <p>Select File to upload</p>
+          {uploading ? (
+            <p>Uploading...</p>
+          ) : (
+            <>
+              <input type="file" onChange={handleChange} />
+              <button onClick={handleUpload}>Upload</button>
+            </>
+          )}
+        </div>
+      </Modal>
     </div>
   );
 }
