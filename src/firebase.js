7292e0f4710b9450aac23cba21f49253d9300ebc
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-03-30 17:03:18 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA05oLi89jPu2JrGf60omQbnVwlmNhGHcA",
-  authDomain: "vistrit-drive.firebaseapp.com",
-  projectId: "vistrit-drive",
-  storageBucket: "vistrit-drive.appspot.com",
-  messagingSenderId: "1019721713372",
-  appId: "1:1019721713372:web:5be1934ac5668733bd1619",
-  measurementId: "G-FY3FPSQ308",
+  // add your own config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
